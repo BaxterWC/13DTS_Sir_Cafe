e72940d6185b56e48dfaaf5a4f7e3d1b5acf2623
BaxterWC <21021@wc.school.nz>
2025-03-05 09:39:54 +1300

    initial commit

--- app.py
+++ app.py
@@ -2,30 +2,44 @@ from flask import Flask, render_template
 import sqlite3
 from sqlite3 import Error
 
-DATABASE = 'cafe_db'
+DATABASE = 'cafe_fr'
 app = Flask(__name__)
 
 
 def connect_to_database(db_file):
     try:
-        con = sqlite3.connect(db_file)
-        return con
+        connection = sqlite3.connect(db_file)
+        return connection
     except Error:
-        print("an error has occured connecting to the database")
+        print("an error has occurred connecting to the database")
     return
+
+
 @app.route('/')
 def render_home():  # put application's code here
     return render_template('home.html')
 
+
 @app.route('/menu')
 def render_menu():  # put application's code here
-    connection = connect_to_database(DATABASE)
+    con = connect_to_database(DATABASE)
     query = "SELECT name, description, volume, image, price FROM products"
-    cur = connection.cursor()
+    cur = con.cursor()
     cur.execute(query)
     product_list = cur.fetchall()
     print(product_list)
+    con.close()
     return render_template('menu.html')
+
+
+@app.route('/contact')
+def render_contact():  # put application's code here
+    return render_template('contact.html')
+
+
+@app.route('/about')
+def render_about():  # put application's code here
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
