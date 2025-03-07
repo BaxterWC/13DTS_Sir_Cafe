8bfd048ccdf6db01d8924927e04a75a2a8117821
BaxterWC <21021@wc.school.nz>
2025-03-07 14:20:57 +1300

    blueberry muffin

--- app.py
+++ app.py
@@ -20,16 +20,20 @@ def render_home():  # put application's code here
     return render_template('home.html')
 
 
-@app.route('/menu')
-def render_menu():  # put application's code here
+@app.route('/menu/<cat_id>')
+def render_menu(cat_id):  # put application's code here
     con = connect_to_database(DATABASE)
-    query = "SELECT name, description, volume, image, price FROM products"
+    query = "SELECT name, description, volume, image, price FROM products WHERE fk_cat_id=?"
+    query_cat_list = "SELECT * FROM categories"
     cur = con.cursor()
-    cur.execute(query)
+    cur.execute(query, (cat_id,))
     product_list = cur.fetchall()
+    cur.execute(query_cat_list)
+    cat_list = cur.fetchall()
     print(product_list)
+    print(cat_list)
     con.close()
-    return render_template('menu.html')
+    return render_template('menu.html', list_of_coffees=product_list, list_of_categories=cat_list)
 
 
 @app.route('/contact')
